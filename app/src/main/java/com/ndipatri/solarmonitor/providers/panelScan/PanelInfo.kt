29c28d6ed704266097f8f09e80ec9e48d37d7aa4
Nicholas DiPatri <ndipatri@gmail.com>
2018-02-24 21:16:36 -0500

    no message

--- app/src/main/java/com/ndipatri/solarmonitor/providers/panelScan/PanelInfo.kt
+++ app/src/main/java/com/ndipatri/solarmonitor/providers/panelScan/PanelInfo.kt
@@ -3,5 +3,6 @@ package com.ndipatri.solarmonitor.providers.panelScan
 data class PanelInfo (val description: String = "new panel",
                       val customerId: String? = null) {
 
-    constructor(attachment: Array<String>) : this(description = attachment[0], customerId = attachment[1])
+    constructor(attachment: Array<String>) : this(description = attachment[0],
+                                                  customerId = attachment[1])
 }
