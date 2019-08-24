d864de335b4c392c24cf98c44d5387e381a6256d
Nicholas DiPatri <ndipatri@gmail.com>
2019-08-24 19:01:40 -0400

    Improved coroutine commentary.

--- app/src/main/java/com/ndipatri/solarmonitor/activities/MainActivityViewModel.kt
+++ app/src/main/java/com/ndipatri/solarmonitor/activities/MainActivityViewModel.kt
@@ -119,16 +119,23 @@ open class MainActivityViewModel(context: Application) : AndroidViewModel(contex
         }
     }
 
+    // Notice this is not a suspending function.  Our Coroutines is launched inside of
+// this method.
     open fun loadSolarOutput() {
 
         scannedPanel?.also {
 
             userState.value = USER_STATE.LOADING
 
+            // This custom CoroutineScope is brought in with "androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0-alpha01"
+            // It automatically will ‘cancel()’ this coroutine if this ViewModel is cleared.
+
             viewModelScope.launch {
 
                 // we're still on main thread here. But now that we're in a coroutine,
-                // this code block itself can be suspended as we call suspend functions.
+                // this code block itself can be suspended as we call suspendable functions.
+                // If this code block suspends, this loadSolarOutput() function returns. If there were code after this
+                // code block, it would execute first and then the function would exit.
 
                 try {
 
@@ -136,6 +143,11 @@ open class MainActivityViewModel(context: Application) : AndroidViewModel(contex
                     // but to call it. 'main safe' means that if this function has to do some
                     // 'background' work, it will switch threads itself either through
                     // 'withContext' or 'async' or 'launch'
+
+
+                    // Used here, async() is another Coroutine launcher.   It returns a Deferred object.
+                    //  This code block does NOT suspend here as we’re not calling a suspend function.
+                    //  Inside the async() block is immediately called on background thread.
                     var powerOutputDeferred = async {solarOutputProvider.getSolarOutput(it.id)}
 
                     // same as above.
@@ -144,12 +156,27 @@ open class MainActivityViewModel(context: Application) : AndroidViewModel(contex
                     val currencyFormat = NumberFormat.getCurrencyInstance()
 
                     var currentProduction = "unavailable"
+
+                    // When we call await() on our Deferred object, this is when our current code block suspends
+                    // until our original ‘getSolarOutput()’ call finishes.
+
+
+                    // This is also the point where an exception will be thrown if an exception is thrown while
+                    // actually executing getSolarOutput() in the background.
+
+                    // If ‘await()’ is called again on the
+                    // same Deferred object,  it will return the value immediately.
                     powerOutputDeferred.await().currentPowerInWatts?.let {
                         // We'll just assume the production of current output for one hour
                         currentProduction = currencyFormat.format(it / 1000 * customerDeferred.await().dollarsPerkWh)
                     }
 
                     var lifetimeProduction = "unavailable"
+
+                    // Same as above, this code block suspends again. Notice the containing ‘loadSolarOutput()’ method
+                    // doesn’t suspend,  just the launcher code  block.
+
+
                     powerOutputDeferred.await().lifetimePowerInWattHours?.let {
                         lifetimeProduction = currencyFormat.format(it / 1000 * customerDeferred.await().dollarsPerkWh)
                     }
@@ -158,12 +185,18 @@ open class MainActivityViewModel(context: Application) : AndroidViewModel(contex
                             .powerOutputMessage.value = "Current ($currentProduction/hour), Annual ($lifetimeProduction)"
 
                     userState.value = USER_STATE.LOADED
+
+                    // Because our await()  calls will throw exceptions as a result  of suspended functions,  we can use a normal
+                    // try/catch block instead of RxJava’s ‘onError()’
+
+
                 } catch (e: Throwable) {
                     userMessage.value = this@MainActivityViewModel.getApplication<Application>().getString(R.string.error_please_try_again)
                     Log.e(TAG, "Exception while loading output.", e)
 
+                    //delay(UI_COMFORT_DELAY)
+
                     // .. then after that delay, we update our UI state.
-                    //delay(1000)
                     resetToSteadyState()
                 }
             }
